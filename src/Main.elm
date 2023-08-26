cc2a009a37e92f985d0fb2e9bacafc8daee3164c
Ben Tyler <email.ben.tyler@gmail.com>
2023-08-26 18:15:01 +1000

    get to work

--- src/Main.elm
+++ src/Main.elm
@@ -1,224 +1,178 @@
 module Main exposing (..)
 
-import Keyboard
-import DrGame exposing (GameObject)
-import DrGrid exposing (GameGridObject)
-import DrSprite exposing (getSprite)
 import Browser
-import Browser.Events exposing (onAnimationFrameDelta, onAnimationFrame)
-import Html exposing (div, img, text)
-import Html.Attributes exposing (height, src, style, width)
-import DrGrid exposing (gameGridObject)
+import Browser.Events exposing (onAnimationFrameDelta)
+import Html exposing (Html, div, text)
+import Keyboard exposing (Key(..))
+import Keyboard.Arrows
 import Time
+import Browser.Events exposing (onAnimationFrame)
+import Html.Attributes exposing (style)
+import Random
 
-----------Move this
-scale = 3
-
-sv : DrSprite.Sprite -> Float -> (Float, Float) -> Html.Html msg
-sv =  DrSprite.viewSprite "0x72_DungeonTilesetII_v1.4.png" scale
-
-draw : GameObject -> Html.Html msg
-draw gameobject =
-    let 
-        drawSprite =
-            sv gameobject.sprite
-               (toFloat gameobject.dir)
-               (toFloat gameobject.x, toFloat gameobject.y)
-     in
-        drawSprite
+dungeonSpriteSheet = "0x72_DungeonTilesetII_v1.4.png"
+scaleConstant = 2
 
 
+itCollides : GameObject -> GameObject -> Bool
+itCollides b1 b2 =
+    let
+        (b1x, b1y )= b1.pos
+        (b2x, b2y) = b2.pos
+        widthCollission = b1x < b2x + b2.w && b1x + b1.w > b2x
+        heightCollision = b1y < b2y + b2.h && b1.h + b1y > b2y
+    in
+    widthCollission && heightCollision
 
------------
+
+doubleList : (GameObject, Sprite) -> List (GameObject, Sprite) -> List (GameObject, Sprite)
+doubleList (toon, toons) inputList =
+    case inputList of
+        [] ->
+            [] 
+
+        (xgo, xgs) :: xs ->
+            if itCollides toon xgo then 
+                doubleList (toon, toons) xs
+            else
+                (xgo, xgs) :: doubleList (toon, toons) xs
+
+pickupExperienceDiamond : Model -> Model
+pickupExperienceDiamond model = 
+    { model | experianceDiamond = doubleList model.knight model.experianceDiamond }
 
 type Msg = 
     Tick Float
-    | LockedTock Time.Posix
+    | LockedTick Time.Posix
     | KeyMsg Keyboard.Msg
+    | AddDiamond Int
+      
+type alias GameObject = 
+    { pos : (Float, Float)
+    , dir : Int
+    , w : Float
+    , h : Float
+    }
 
 type alias Model =
     { ticks: Int
-    , fps: Float 
-    , pressedKeys: List Keyboard.Key
-    , gameGrid: List GameGridObject
-    , snake: List GameGridObject
-    , food: List GameGridObject
-    , direction: (Int, Int)
-    }
+     , fps: Float
+     , pressedKeys: List Key
+     , knight :  (GameObject, Sprite)
+     , experianceDiamond : List (GameObject, Sprite)
+     }
 
-makeSnake : (Int, Int) -> GameGridObject
-makeSnake (x, y) = 
-            getSprite 192 68 16 28 4
-            |> DrGrid.gameGridObject (x, y)
-
-init : a -> (Model, Cmd msg)
+init : a -> (Model, Cmd Msg)
 init _ =
-    let
-        food loc = 
-            getSprite 288 224 16 16 1
-            |> DrGrid.gameGridObject loc
-    in
     ( { ticks = 1
       , fps = 0.0
       , pressedKeys = []
-      , gameGrid = getSprite 16 16 16 16 1
-                   |> DrGrid.gameGrid
-      , snake = [ makeSnake (1, 0), makeSnake (0, 0)]
-      , food = 
-        [ food (3, 0)
-        , food (5, 0)
-        , food (3, 3)
-        , food (0, 0)
-        , food (6, 6)
-        , food (8, 0)
-        , food (0, 8)
-        , food (2, 5)
-        , food (6, 3)
-        , food (5, 5)
-        , food (8, 2)
-        ]
-      , direction = (1, 0)
+      , knight = 
+        ( GameObject (100, 100) 0 20 20
+        , Sprite 192 68 16 28 4 0
+        )
+      , experianceDiamond = []
       }
-    , Cmd.none
+      , Cmd.batch
+        [ Random.generate AddDiamond (Random.int 1 400)
+        , Random.generate AddDiamond (Random.int 1 400)
+        ]
     )
 
+
+
+addDiamond : Model -> Int -> Model
+addDiamond model randomNumber = 
+    { model 
+        | experianceDiamond = 
+            ( GameObject (toFloat randomNumber, toFloat randomNumber) 0 20 20
+            , Sprite 288 320 16 16 1 0
+            ) :: model.experianceDiamond 
+    }
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch <|
+        [ onAnimationFrameDelta Tick
+        , onAnimationFrame LockedTick
+        , Sub.map KeyMsg Keyboard.subscriptions
+        ]                 
+       
+viewGameObject (gameobject, sprite) =
+    viewSprite 
+        dungeonSpriteSheet 
+        scaleConstant 
+        sprite 
+        ( gameobject.dir |> toFloat )
+        gameobject.pos
+
 view : Model -> Html.Html msg
-
 view model =
-    let 
-        drawFood = 
-            case model.food of 
-                x::_ -> [draw x.gameObject]
-                [] -> [div [] []]
-    in   
     div []
-        (  div [] [ text <| String.fromInt model.ticks ] 
-           :: List.map (\i -> draw i.gameObject) model.gameGrid 
-           ++ drawFood
-          -- ++ List.map (\i -> draw i.gameObject ) model.food
-           ++ List.map( \i -> draw i.gameObject) model.snake
-        )
+        [ viewGameObject model.knight
+       ,  div [] (List.map viewGameObject model.experianceDiamond )
+        , div [] [ text
+                       <| "fps: " ++ String.fromFloat model.fps ]
+        , div [] [ text
+                       <| "ticks: " ++ String.fromInt model.ticks ]
+        , div [] [ text (Debug.toString model)]     
+        ]
 
-handleSnake : Model -> Model
-handleSnake model =
-    let
-        _ = Debug.log "snek" (List.length model.snake)
-        getCurrentFoodLocation = 
-            case model.food of
-                f::_ -> f.grid
-                [] -> (99, 99)
+animate (go, sprite) = 
+    if sprite.currentFrame == 4 then 
+        (go, { sprite | currentFrame = 0 })
+    else
+       (go, { sprite | currentFrame = sprite.currentFrame + 1 })
 
-        (dx, dy)= model.direction
-
-        newLoc (x, y) = 
-            (x+ dx, y + dy)
- 
-        move ggo = 
-            DrGrid.smoothSetGoo 
-                (newLoc ggo.grid)
-                 ggo
-
-        -- item moves to the head of the snek
-        smf: List GameGridObject -> Maybe (Int, Int) -> List GameGridObject -> List GameGridObject
-        smf accum prev snake = 
-            case snake of
-                [] -> accum
-                x::xs -> 
-                    case prev of
-                        Nothing -> smf [move x] (Just x.grid) xs
-                        Just ploc -> 
-                            smf 
-                                (accum ++ [(DrGrid.smoothSetGoo ploc x)])
-                                (Just x.grid)
-                                xs
-
-
-        tryToEat snake = 
-            case snake of 
-                snakeHead::r ->
-                    let
-                        (tx, ty) = 
-                            case List.reverse snake of 
-                                x::_ -> x.grid
-                                [] -> (9, 9)
-                    in
-                    if snakeHead.grid == getCurrentFoodLocation then
-                        snakeHead :: r ++ [makeSnake (tx - dx, ty - dy)]
-                    else 
-                        snakeHead::r
-                    --(onFood h) ++ r
-                [] -> []
-
-        nextFood = 
-            if List.length (tryToEat model.snake) /=  List.length model.snake then 
-                case model.food of 
-                    _::r -> r
-                    [] -> []
-            else
-                model.food 
-
-        
-   in
-   { model 
-   | snake =
-         smf [] Nothing model.snake
-         |> tryToEat
-    , food = nextFood
-   -- , direction = (dx, dy)
-   }
-
-tick : Float -> Model -> Model
-tick delta model =
-    let
-        (xdir, _) = model.direction
-        animated = 
-            List.map DrGrid.smoothMoveGgo model.snake
-            |> List.map (\ i -> DrGrid.animateGameGridObject i model.ticks)
-            |> List.map( \i -> DrGrid.setDir i xdir)
-
-        animatedModel = 
-            { model
-            | snake = animated
-            }
-
-        snakeHandled = handleSnake animatedModel
-            
-        handleDirection = 
-            case DrGame.moveOnKeyBoard model.pressedKeys of 
-                (0, 0) -> model.direction
-                (_, _) -> DrGame.moveOnKeyBoard model.pressedKeys
-
-    in
-    if modBy 200 model.ticks == 0 then
-       { snakeHandled
-       | direction = handleDirection
-       }
-    else 
-        { animatedModel
-        | direction = handleDirection 
-        }
-        
+handleFramerate : Int -> Bool
+handleFramerate ticks = 
+    modBy 5 ticks == 0
 
 update : Msg -> Model -> (Model, Cmd msg)
 update msg model =
     case msg of
-        LockedTock posix -> 
-            ( tick 
-                1.0 
-                ({ model | ticks = model.ticks + 1 })
-            , Cmd.none
-            )
+        AddDiamond randomNumber -> 
+            (addDiamond model randomNumber, Cmd.none)
 
-        Tick delta ->
-            --( tick 
-            --    delta 
-            --    ({ model | ticks = model.ticks + 1 })
-            --, Cmd.none
-            --)
-            (model, Cmd.none)
-       
+        Tick _ -> 
+            ( { model | ticks = model.ticks + 1 }, Cmd.none )
+        
         KeyMsg keyMsg ->
-          ({ model | pressedKeys = Keyboard.update keyMsg model.pressedKeys}, Cmd.none)
+            ( { model | pressedKeys = Keyboard.update keyMsg model.pressedKeys}, Cmd.none )
+        
+        LockedTick posix -> 
+            if handleFramerate model.ticks then 
+                ( { model 
+                    | ticks = model.ticks + 1 
+                    , knight = 
+                        animate model.knight
+                            |> moveOnKeyBoard model.pressedKeys
+                  }
+                    |> pickupExperienceDiamond
+                , Cmd.none
+                )
+            else 
+                (model, Cmd.none)
+
+
+moveOnKeyBoard : List Keyboard.Key -> (GameObject, Sprite) -> (GameObject, Sprite)
+moveOnKeyBoard pressedKeys (go, sprite) =
+    let
+        arrows =
+            Keyboard.Arrows.arrows pressedKeys
+
+        (cx, cy) = go.pos
+        newGo = 
+            { go 
+                | pos =  
+                    ( ( arrows.x * 20 |> toFloat) + cx 
+                    , ( arrows.y * -1 * 20 |> toFloat) + cy
+                    )
+                , dir = if arrows.x == 0 then go.dir else arrows.x
+            }
+    in
+    (newGo, sprite)
+   
 
 
 main : Program () Model Msg
@@ -231,11 +185,52 @@ main =
         }
 
 
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.batch <|
-        [ onAnimationFrameDelta Tick
-        , onAnimationFrame LockedTock
-        , Sub.map KeyMsg Keyboard.subscriptions
+type alias Sprite =
+    { x : Float
+    , y : Float
+    , w : Float
+    , h : Float
+    , frames : Float
+    , currentFrame : Float
+    }
+
+setSpriteSheet : Sprite -> String
+setSpriteSheet sprite =
+    "-" ++ String.fromFloat (sprite.x + sprite.currentFrame * sprite.w) ++ "px" ++ " -" ++ String.fromFloat sprite.y ++ "px"
+
+
+drawPosition : ( Float, Float ) -> List (Html.Attribute msg)
+drawPosition ( x, y ) =
+    [ style "position" "absolute"
+    , style "left" <| String.fromFloat x ++ "px"
+    , style "top" <| String.fromFloat y ++ "px"
+    ]
+
+scaleSprite : Float -> String -> String
+scaleSprite factor direction =
+    "scale("
+        ++ String.fromFloat factor
+        ++ ") scaleX("
+        ++ direction
+        ++ ")"
+
+viewSprite : String -> Float -> Sprite -> Float -> ( Float, Float ) -> Html msg
+viewSprite spriteSheet scale sprite dir ( xloc, yloc ) =
+    let
+        spriteDirection =
+            if dir == 0 then
+                String.fromFloat 1
+
+            else
+                String.fromFloat dir
+    in
+    div (drawPosition ( xloc, yloc ))
+        [ div
+            [ style "background" <| "url(" ++ spriteSheet ++ ") no-repeat"
+            , style "background-position" <| setSpriteSheet sprite
+            , style "width" <| String.fromFloat sprite.w ++ "px"
+            , style "height" <| String.fromFloat sprite.h ++ "px"
+            , style "transform" <| scaleSprite scale spriteDirection
+            ]
+            []
         ]
-        
